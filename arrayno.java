9fa308e78efd48cc72992e778495656e50f30550
madhanrajR <madhanit31@gmail.com>
2016-07-23 10:31:11 +0530

    Add files via upload

---
+++ arrayno.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+public class arrayno {
+public static void main(String[] args) {
+	int[] a=new int[7];
+	Scanner nc =new Scanner(System.in);
+	System.out.println("enter the digit:");
+	for(int d=0;d<a.length;d++)
+	{
+		a[d]=nc.nextInt();
+	}
+	for(int i=0;i<a.length;i++)
+	{
+		for(int j=i+1;j<a.length;j++)
+		{
+			if(a[i]==a[j])
+			{
+				a[i]=0;
+				a[j]=0;
+			}
+			
+		}
+	}
+	for(int e=0;e<a.length;e++)
+	{
+		if(a[e]!=0)
+		{
+			System.out.println("return:"+a[e]);
+		}
+	}
+	
+	}
+}
+
